67d4a2cb9467404770cc048515392062e82a5c1c
Jay Liu <jayl252574@gmail.com>
2020-12-28 18:46:58 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
 
   name_hash.each do |key, value|
 
-    if current_min_value == nil 
+    if current_min_value == nil
       current_min_value = value
       current_key_assoc_w_min_value = key
     else
@@ -15,7 +15,7 @@ def key_for_min_value(name_hash)
         current_min_value = value
         current_key_assoc_w_min_value = key
     end
-   end 
+   end
 end
 current_key_assoc_w_min_value
-end 
+end
